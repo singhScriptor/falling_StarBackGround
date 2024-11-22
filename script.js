ab61d1e4b8e94da9a42359963e3b179b4fff9e58
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-11-22 23:13:52 +0530

    skyfall animation background

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const stars = document.querySelectorAll(".star");
+    stars.forEach(star => {
+        star.style.setProperty('--random-top', Math.random());
+        star.style.setProperty('--fall-duration', `${6 + Math.random() * 6}s`);
+        star.style.setProperty('--fall-delay', `${Math.random() * 10}s`);
+    });
+});
